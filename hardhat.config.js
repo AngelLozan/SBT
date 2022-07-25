b9f60cd880c182676f206406616a39766855f06a
Scott Lozano <lozans89@gmail.com>
2022-07-25 12:00:23 +0100

    updates testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,14 +8,15 @@ require("@nomiclabs/hardhat-etherscan");
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
-  defaultNetwork: "rinkeby",
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
+      chainId: 1337
     },
-    // rinkeby: {
-    //   url: API_URL,
-    //   accounts: [`0x${PRIVATE_KEY}`]
-    // },
+    rinkeby: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
     mumbai: {
       url: "https://polygon-mumbai.infura.io/v3/3d67dbef6f8b488780529a324416cb0d",
       accounts: [`0x${PRIVATE_KEY}`]
